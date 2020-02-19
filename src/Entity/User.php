c1545237ee6a5c635450c1a81347475b3764d3b2
Thomas Claireau <thomas.claireau@gmail.com>
2020-02-19 19:02:15 +0100

    add entity client / product / user

---
+++ src/Entity/User.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+	/**
+	 * @ORM\Id()
+	 * @ORM\GeneratedValue()
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
+
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $email;
+
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $name;
+
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $role;
+
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $password;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity="App\Entity\Client", inversedBy="user")
+	 */
+	private $client;
+
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
+
+	public function getEmail(): ?string
+	{
+		return $this->email;
+	}
+
+	public function setEmail(string $email): self
+	{
+		$this->email = $email;
+
+		return $this;
+	}
+
+	public function getName(): ?string
+	{
+		return $this->name;
+	}
+
+	public function setName(string $name): self
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	public function getRole(): ?string
+	{
+		return $this->role;
+	}
+
+	public function setRole(string $role): self
+	{
+		$this->role = $role;
+
+		return $this;
+	}
+
+	public function getPassword(): ?string
+	{
+		return $this->password;
+	}
+
+	public function setPassword(string $password): self
+	{
+		$this->password = $password;
+
+		return $this;
+	}
+
+	public function getClient(): ?Client
+	{
+		return $this->client;
+	}
+
+	public function setClient(?Client $client): self
+	{
+		$this->client = $client;
+
+		return $this;
+	}
+}
